265155a6bcd22d8ac13188214116d67d5dc95118
Eduardo Bueno Muniz de Souza <edubms@MacBook-Pro-de-Eduardo.local>
2024-04-03 22:05:37 -0300

    updating the validators

--- src/shelter/usecases/dtos/update.shelter.controller.input.ts
+++ src/shelter/usecases/dtos/update.shelter.controller.input.ts
@@ -1,6 +1,19 @@
+import { IsEmail, IsNotEmpty, IsNumberString, IsString, Length } from "class-validator"
+import { isString } from "util"
+
 export default class UpdateShelterControllerInput {
+    @IsNotEmpty()
+    @IsString()
     shelterName: string
+    @IsNotEmpty()
+    @IsNumberString()
+    @Length(10,11)
     shelterWhatsapp:string
+    @IsNotEmpty()
+    @IsNumberString()
+    @Length(10,11)
     shelterPhone: string
+    @IsNotEmpty()
+    @IsEmail()
     shelterEmail: string
 }
